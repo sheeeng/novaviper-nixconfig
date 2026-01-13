8e8c7fbe4bba06f0238a117cc50f4866624b48d7
NovaViper <coder.nova99@mailbox.org>
2026-01-12 22:59:59 -0600

    knoxpc: add wifi network config
    For some reason recently it no longer used the local config.. so now
    making it declarative and hidden

--- hosts/knoxpc/config/wifi.nix
+++ hosts/knoxpc/config/wifi.nix
@@ -1,4 +1,24 @@
-{ lib, ... }:
 {
+  config,
+  lib,
+  myLib,
+  ...
+}:
+{
+  sops.secrets."wifi.conf" = myLib.secrets.mkSecretFile {
+    source = "wifi.conf";
+    format = "binary";
+    subDir = [
+      "hosts"
+      "${config.networking.hostName}"
+    ];
+  };
+
   networking.wireless.enable = true;
+
+  networking.wireless.interfaces = [ "wlp2s0" ];
+
+  networking.wireless.secretsFile = config.sops.secrets."wifi.conf".path;
+
+  networking.wireless.networks."ext:home_ssid".pskRaw = "ext:home_pskRaw";
 }
